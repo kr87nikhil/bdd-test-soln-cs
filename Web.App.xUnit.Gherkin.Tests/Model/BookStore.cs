e7ea6ddf37ea19b08594da79ab0f8fb34c91e4a9
Nikhil Kumar <nikhil1552@gmail.com>
2024-05-30 11:16:57 +0530

    Book Store: Record, API definition and Authenticator added

---
+++ Web.App.xUnit.Gherkin.Tests/Model/BookStore.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Web.App.xUnit.Gherkin.Tests.Web.Model;
+
+internal record Login(string UserName, string Password);
+/// <summary>
+/// Receives Generated Token & User Id of logged in User
+/// </summary>
+/// <param name="Password">Private accessor to restrict external usage</param>
+internal record GeneratedToken(string UserId, string Username, string Password, string Token, DateTime Expires, 
+    [property: JsonPropertyName("created_date")] DateTime CreatedDate, bool IsActive);
+
+internal record BookIsbn(string Isbn);
+internal record BookRequest(string UserId, IList<BookIsbn> CollectionOfIsbns);
+internal record ReturnRequest(string UserId, string Isbn);
+
+internal record Book(string Isbn, string Title, string SubTitle, string Author, 
+    [property: JsonPropertyName("publish_date")] DateTime PublishDate, string Publisher, int Pages, string Description, Uri Website);
+internal record BorrowedBooks(string UserId, string Username, IList<Book> Books);
---
+++ Web.App.xUnit.Gherkin.Tests/Model/RestSharp/ToolsQaAuthenticator.cs
@@ -0,0 +1,25 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+
+namespace Web.App.xUnit.Gherkin.Tests.Model.RestSharpClient;
+
+internal class ToolsQaAuthenticator(string Username, string Password) : AuthenticatorBase(string.Empty)
+{
+    protected override async ValueTask<Parameter> GetAuthenticationParameter(string accessToken)
+    {
+        Token = string.IsNullOrEmpty(accessToken) ? await GetToken() : Token;
+        return new HeaderParameter(KnownHeaders.Authorization, Token);
+    }
+
+    /// <summary>
+    /// Generate token for ToolQaClient
+    /// </summary>
+    /// <returns>Bearer Token</returns>
+    private async Task<string> GetToken()
+    {
+        using var client = new RestClient(ToolsQaClient.DemoQaBaseUrl);
+        var jwtToken = await ToolsQaApiDefinition.PerformLogin(client, Username, Password);
+        ToolsQaClient.LoggedInUserId = jwtToken.UserId;
+        return $"Bearer {jwtToken.Token}";
+    }
+}
