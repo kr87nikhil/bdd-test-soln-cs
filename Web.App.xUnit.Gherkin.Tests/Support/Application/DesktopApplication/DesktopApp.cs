698a75dc7e74a91a335bc19236984c14d7ca0dbd
Nikhil Kumar <nikhil1552@gmail.com>
2024-06-09 18:37:42 +0530

    Moved towards generic Application: Device browser factory introduced

---
+++ Web.App.xUnit.Gherkin.Tests/Support/Application/DesktopApplication/DesktopApp.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.App.xUnit.Gherkin.Tests.Support.Application.DesktopApplication;
+
+internal abstract class DesktopApp
+{
+}
+
+internal class MacDesktopApp : DesktopApp
+{
+}
+
+internal class WindowsDesktopApp : DesktopApp
+{
+}
---
+++ Web.App.xUnit.Gherkin.Tests/Support/Application/WebApplication/DesktopBrowserFactory.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Safari;
+using Web.App.xUnit.Gherkin.Tests.Support.Model.Browser;
+
+namespace Web.App.xUnit.Gherkin.Tests.Support.Application.WebApplication;
+internal class DesktopBrowserFactory(DriverOptions WebDriverOptions, bool HeadlessMode) : IDeviceBrowserFactory
+{
+    public EdgeBrowser GetEdgeBrowser =>
+        new EdgeBrowser((EdgeOptions)WebDriverOptions).EnableHeadless(HeadlessMode);
+
+    public SafariBrowser GetSafariBrowser => new((SafariOptions)WebDriverOptions);
+
+    public ChromeBrowser GetChromeBrowser =>
+        new ChromeBrowser((ChromeOptions)WebDriverOptions).EnableHeadless(HeadlessMode);
+
+    public FirefoxBrowser GetFirefoxBrowser =>
+        new FirefoxBrowser((FirefoxOptions)WebDriverOptions).EnableHeadless(HeadlessMode);
+}
