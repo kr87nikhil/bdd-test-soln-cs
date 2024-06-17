a50844a5d34d9b3b32749c8345658234b398ff23
Nikhil Kumar <nikhil1552@gmail.com>
2024-06-17 10:49:54 +0530

    App configuration: Extension hosting package with appsettings.json added

---
+++ Web.App.xUnit.Gherkin.Tests/Support/Model/DeviceConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace Web.App.xUnit.Gherkin.Tests.Support.Model;
+
+public record DesktopLaptopBrowserConfiguration
+{
+    public required bool HeadlessExecution { get; set; }
+    public required Uri SeleniumGridUrl { get; set; }
+    public required bool SeleniumGridExecution { get; set; }
+    public required IList<BrowserConfiguration> LocalBrowser { get; set; }
+    public required string LocalBrowserBinaryExecutablePath { get; set; }
+}
+
+public record BrowserConfiguration
+{
+    public required string Name { get; set; }
+    public string? Version { get; set; }
+    public string? DriverVersion { get; set; }
+}
